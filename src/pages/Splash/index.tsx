3c9bd9d5febfded03df6c585884472fc5f0a6a50
ryanvincere <ryanadhitama2@gmail.com>
2023-08-20 10:17:36 +0800

    react native svg

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Logo } from '@assets';
 
 const Splash = () => {
   return (
     <View style={styles.page}>
+      <Logo />
       <Text style={styles.title}>Splash</Text>
     </View>
   );
