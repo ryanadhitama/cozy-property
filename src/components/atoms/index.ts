6bec11aa0bf98760aa3666e835cb644ab7349a4e
ryanvincere <ryanadhitama2@gmail.com>
2023-08-23 19:54:24 +0800

    home title

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './Button';
 export { default as TabItem } from './TabItem';
+export { default as Gap } from './Gap';
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,19 @@
-import { Header } from '@components';
-import { colors } from '@utils';
+import { Gap, Header } from '@components';
+import { colors, fonts } from '@utils';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = () => {
   return (
     <SafeAreaView style={styles.page}>
       <Header title="Explore Now" desc="Mencari kosan yang cozy" />
+      <Text style={styles.title}>Popular Cities</Text>
+      <Gap height={16} />
+      <Text style={styles.title}>Recommended Space</Text>
+      <Gap height={16} />
+      <Text style={styles.title}>Tips & Guidance</Text>
+      <Gap height={16} />
     </SafeAreaView>
   );
 };
@@ -18,5 +24,10 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: colors.white
+  },
+  title: {
+    paddingHorizontal: 24,
+    fontSize: 16,
+    fontFamily: fonts.primary.normal
   }
 });
