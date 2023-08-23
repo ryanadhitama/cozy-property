ac17272ea978dc74a7656f744e7f17bd4b43f4d8
ryanvincere <ryanadhitama2@gmail.com>
2023-08-23 21:35:53 +0800

    bottom navigation

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,4 +1,23 @@
 import Logo from './logo.svg';
 import NotFound from './404.svg';
+import Home from './home.svg';
+import HomeActive from './home-active.svg';
+import Message from './message.svg';
+import MessageActive from './message-active.svg';
+import News from './news.svg';
+import NewsActive from './news-active.svg';
+import Wishlist from './wishlist.svg';
+import WishlistActive from './wishlist-active.svg';
 
-export { Logo, NotFound };
+export {
+  Logo,
+  NotFound,
+  Home,
+  HomeActive,
+  Message,
+  MessageActive,
+  News,
+  NewsActive,
+  Wishlist,
+  WishlistActive
+};
--- src/components/atoms/TabItem/index.tsx
+++ src/components/atoms/TabItem/index.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
-import { fonts } from '../../../utils';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  Home,
+  HomeActive,
+  Message,
+  MessageActive,
+  News,
+  NewsActive,
+  Wishlist,
+  WishlistActive
+} from '@assets';
 
 type TabItemType = { title: string; active: boolean; onPress: () => void; onLongPress: () => void };
 
 const TabItem = ({ title, active, onPress, onLongPress }: TabItemType) => {
+  const Icon = () => {
+    if (title === 'Home') {
+      return active ? <HomeActive /> : <Home />;
+    }
+    if (title === 'Messages') {
+      return active ? <MessageActive /> : <Message />;
+    }
+    if (title === 'News') {
+      return active ? <NewsActive /> : <News />;
+    }
+    if (title === 'Wishlist') {
+      return active ? <WishlistActive /> : <Wishlist />;
+    }
+    return <Home />;
+  };
   return (
-    <TouchableOpacity style={styles.container} onPress={onPress} onLongPress={onLongPress}>
-      <Text style={styles.text(active)}>{title}</Text>
+    <TouchableOpacity
+      style={[styles.container, { borderBottomColor: active ? '#7F74EB' : 'transparent' }]}
+      onPress={onPress}
+      onLongPress={onLongPress}
+    >
+      <Icon />
     </TouchableOpacity>
   );
 };
@@ -15,10 +43,5 @@ const TabItem = ({ title, active, onPress, onLongPress }: TabItemType) => {
 export default TabItem;
 
 const styles = StyleSheet.create({
-  container: { alignItems: 'center' },
-  text: (_) => ({
-    fontSize: 10,
-    fontFamily: fonts.primary[600],
-    marginTop: 4
-  })
+  container: { alignItems: 'center', paddingVertical: 20, borderBottomWidth: 4 }
 });
